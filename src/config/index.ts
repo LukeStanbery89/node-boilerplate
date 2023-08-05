cd8d4c1bfd67ad219c78d5277418f3bf199eeb24
Luke Stanbery <lukestanbery89@gmail.com>
2023-08-05 16:54:01 -0500

    refactor rate limiter config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,8 @@ dotenv.config();
 
 export default {
     PORT: process.env.PORT || 3000,
+    RATE_LIMITER: {
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // Limit each IP to 100 requests per windowMs
+    },
 };
