1bced95be163f6f2989130f61f15d5f29ca068d7
Luke Stanbery <LukeStanbery89@users.noreply.github.com>
2023-08-05 16:59:22 -0500

    refactor rate limiter config (#2)

--- src/loaders/rateLimiter.ts
+++ src/loaders/rateLimiter.ts
@@ -1,11 +1,8 @@
 import { Express } from "express";
 import rateLimit from "express-rate-limit";
+import config from "../config";
 
-// Define the rate limit options
-const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
-});
+const limiter = rateLimit(config.RATE_LIMITER);
 
 export default function initRateLimiter(app: Express) {
     app.use(limiter);
